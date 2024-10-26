0fa0608337f7afe9d140963519292c52bc28fe8b
assafv <assaf.vilmovski@gmail.com>
2024-10-26 14:37:21 +0300

    stage 4: added stylesheets and media support by endpoint

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 var http = require('http');
-
-var message = 'I am so happy to be part of this workshop!';
+var fs = require('fs');
 
 function handler (request, response) {
     
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message); //response body
-    response.end(); // finish response
+    var endpoint = request.url;
 
+    if (endpoint === "/") {
+        response.writeHead(200, {"Content-Type": "text/html"});
+    
+        fs.readFile(__dirname + '/public/index.html', function(error, file) {
+          if (error) {
+            console.log(error);
+            return;
+          }
+    
+          response.end(file);
+        });
+    } else if (endpoint.includes("image")) {
+        response.writeHead(200, {"Content-Type": "image/jpeg"});
+        fs.readFile(__dirname + '/public/' + endpoint, function(error, file) {
+            if (error) {
+              console.log(error);
+              return;
+            }
+      
+            response.end(file);
+          });
+
+      } else if (endpoint.includes(".css")) {
+        response.writeHead(200, {"Content-Type": "text/css"});
+        fs.readFile(__dirname + '/public/' + endpoint, function(error, file) {
+          if (error) {
+            console.log(error);
+            return;
+          }
+    
+          response.end(file);
+        });
+
+      }
+      
+      else {
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.write("Not found");
+        response.end()
+      }
 }
 
 var server = http.createServer(handler);
